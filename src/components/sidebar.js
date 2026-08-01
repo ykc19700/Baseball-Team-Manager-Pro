e9ced11bdc9612488bff2f4a48f7c45e3d2fde3c
ykc19700 <ykc19700@gmail.com>
2026-08-01 09:14:36 +0500

    Add sidebar component

---
+++ src/components/sidebar.js
@@ -0,0 +1,12 @@
+export function renderSidebar(element) {
+  element.innerHTML = `
+    <nav>
+      <ul>
+        <li>Dashboard</li>
+        <li>Players</li>
+        <li>Lineup</li>
+        <li>Game Day</li>
+      </ul>
+    </nav>
+  `;
+}
