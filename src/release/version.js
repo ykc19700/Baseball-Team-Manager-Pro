57800c22e323b1736f736b226ad5e26c17940e06
ykc19700 <ykc19700@gmail.com>
2026-08-01 09:32:42 +0500

    Add release version information

---
+++ src/release/version.js
@@ -0,0 +1,12 @@
+export const APP_VERSION = {
+  name: 'Baseball Team Manager Pro',
+  version: '1.0.0',
+  release: 'Coach Edition',
+  features: [
+    'Player Management',
+    'Smart Starting Order',
+    'Game Day',
+    'Statistics',
+    'PWA Offline Support'
+  ]
+};
