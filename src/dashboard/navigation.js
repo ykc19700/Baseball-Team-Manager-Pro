1d0a5664348aa3ead0420e80adb96a3ed09a884a
ykc19700 <ykc19700@gmail.com>
2026-08-01 09:21:07 +0500

    Add dashboard navigation module

---
+++ src/dashboard/navigation.js
@@ -0,0 +1,18 @@
+export const MENU_ITEMS = [
+  {
+    id: 'players',
+    name: '球員管理'
+  },
+  {
+    id: 'lineup',
+    name: '先發 Order'
+  },
+  {
+    id: 'game',
+    name: 'Game Day'
+  },
+  {
+    id: 'report',
+    name: '報表'
+  }
+];
