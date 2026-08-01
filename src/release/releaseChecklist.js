bdc1be5dbf0afb81f8319d21020efd5f7cd051d5
ykc19700 <ykc19700@gmail.com>
2026-08-01 09:32:50 +0500

    Add release checklist

---
+++ src/release/releaseChecklist.js
@@ -0,0 +1,8 @@
+export const RELEASE_CHECKLIST = [
+  'Player Management completed',
+  'Starting Order completed',
+  'Game Day completed',
+  'Statistics module ready',
+  'PWA support enabled',
+  'GitHub Pages deployment ready'
+];
