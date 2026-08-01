927f4037ec93c294528955e7897c903afa8c8bdf
ykc19700 <ykc19700@gmail.com>
2026-08-01 09:25:43 +0500

    Add PWA service worker

---
+++ service-worker.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'baseball-manager-v5';
+const FILES = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
